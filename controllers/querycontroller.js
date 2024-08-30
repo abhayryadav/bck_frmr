a82ffc4d2d224d38087ae6dd26f4adef3a502359
Abhay Raj Yadav <abhayryadav0007@gmail.com>
2024-08-30 21:01:04 +0530

    modif17

--- controllers/querycontroller.js
+++ controllers/querycontroller.js
@@ -107,7 +107,7 @@ async function submitQuery(req, res) {
 
         // Await the sendMail promise
         const emailResponse = await sendMail(
-            'abhayryadav0007@gmail.com',
+            'info@farmerlegacybiotech.com',
             `webquerry from ${body.email} , name - ${body.name}`,
             `
                 <div style="font-family: Arial, sans-serif; line-height: 1.6; text-align: center; max-width: 600px; margin: 40px auto; padding: 20px; border-radius: 20px; box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2); background-color: #f9f9f9;">
