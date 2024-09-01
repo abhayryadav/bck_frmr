79f367a83498050fecdf6ec5090fdc1e0955c651
Abhay Raj Yadav <abhayryadav0007@gmail.com>
2024-09-01 19:10:41 +0530

    modif28

--- index.js
+++ index.js
@@ -17,49 +17,52 @@ app.use(bodyParser.json());
 
 
 
-const corsOptions = {
-    origin: ['http://www.farmerlegacybiotech.com', 'https://farmerlegacybiotech.com','https://tobereviewed-2-lg1d.vercel.app/'],
-    methods: ['GET', 'POST', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-  };
+// const corsOptions = {
+//     origin: ['http://www.farmerlegacybiotech.com', 'https://farmerlegacybiotech.com','https://tobereviewed-2-lg1d.vercel.app/'],
+//     methods: ['GET', 'POST', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//     credentials: true
+//   };
   
-const allowedOrigins = ['http://www.farmerlegacybiotech.com', 'https://farmerlegacybiotech.com', 'https://tobereviewed-2-lg1d.vercel.app'];
-// middleware
-app.use((req, res, next) => {
-    const origin = req.get('Origin');
-    if (allowedOrigins.includes(origin)) {
-        res.header("Access-Control-Allow-Origin", origin);
-    }
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(204); // Respond successfully to OPTIONS request
-    }
-    next();
-});
+// const allowedOrigins = ['http://www.farmerlegacybiotech.com', 'https://farmerlegacybiotech.com', 'https://tobereviewed-2-lg1d.vercel.app'];
+// // middleware
+// app.use((req, res, next) => {
+//     const origin = req.get('Origin');
+//     if (allowedOrigins.includes(origin)) {
+//         res.header("Access-Control-Allow-Origin", origin);
+//     }
+//     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+//     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//     if (req.method === 'OPTIONS') {
+//         return res.sendStatus(204); // Respond successfully to OPTIONS request
+//     }
+//     next();
+// });
 
 
 
 // same as above but bit advanced
-// const allowedOrigins = [
-//     'http://www.farmerlegacybiotech.com',
-//     'https://farmerlegacybiotech.com',
-//     'https://tobereviewed-2-lg1d.vercel.app'
-// ];
+const allowedOrigins = [
+    'http://www.farmerlegacybiotech.com',
+    'https://farmerlegacybiotech.com',
+    'https://tobereviewed-2-lg1d.vercel.app'
+];
 
-// const corsOptions = {
-//     origin: (origin, callback) => {
-//         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-//             callback(null, true);
-//         } else {
-//             callback(new Error('Not allowed by CORS'));
-//         }
-//     },
-//     methods: ['GET', 'POST', 'OPTIONS'],
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-//     credentials: true
-// };
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+};
+
+
+
 
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
