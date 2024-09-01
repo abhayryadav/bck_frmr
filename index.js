e96410a6abf89638a64c98085e3ddffed442c92a
Abhay Raj Yadav <abhayryadav0007@gmail.com>
2024-09-01 18:57:35 +0530

    modif26

--- index.js
+++ index.js
@@ -8,32 +8,57 @@ const app = express();
 const port = 4000;
 
 // Middleware setup
-app.use(express.json());
+
 app.use(bodyParser.json());
 // app.use(cors());
+// const corsOptions = {
+//     origin: ['http://www.farmerlegacybiotech.com', 'https://farmerlegacybiotech.com','https://tobereviewed-2-lg1d.vercel.app/'],
+//     methods: ['GET', 'POST', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//     credentials: true
+//   };
+  
+// app.use(cors(corsOptions));
+// app.options('*', cors(corsOptions));  // Enable pre-flight for all routes
+// const allowedOrigins = ['http://www.farmerlegacybiotech.com', 'https://farmerlegacybiotech.com', 'https://tobereviewed-2-lg1d.vercel.app'];
+// // middleware
+// app.use((req, res, next) => {
+//     const origin = req.get('Origin');
+//     if (allowedOrigins.includes(origin)) {
+//         res.header("Access-Control-Allow-Origin", origin);
+//     }
+//     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+//     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//     if (req.method === 'OPTIONS') {
+//         return res.sendStatus(204); // Respond successfully to OPTIONS request
+//     }
+//     next();
+// });
+
+const allowedOrigins = [
+    'http://www.farmerlegacybiotech.com',
+    'https://farmerlegacybiotech.com',
+    'https://tobereviewed-2-lg1d.vercel.app'
+];
+
 const corsOptions = {
-    origin: ['http://www.farmerlegacybiotech.com', 'https://farmerlegacybiotech.com','https://tobereviewed-2-lg1d.vercel.app/'],
+    origin: (origin, callback) => {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
-  };
-  
+};
+
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));  // Enable pre-flight for all routes
-const allowedOrigins = ['http://www.farmerlegacybiotech.com', 'https://farmerlegacybiotech.com', 'https://tobereviewed-2-lg1d.vercel.app'];
-// middleware
-app.use((req, res, next) => {
-    const origin = req.get('Origin');
-    if (allowedOrigins.includes(origin)) {
-        res.header("Access-Control-Allow-Origin", origin);
-    }
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(204); // Respond successfully to OPTIONS request
-    }
-    next();
-});
+app.options('*', cors(corsOptions));
+app.use(express.json());
+
+
 
 app.use('/static', express.static(path.join(__dirname, 'images')));
 
